eaa5cbdfed479432722496d73a3175b7baa3c1d5
Erfan Rasti <erfanrasty@gmail.com>
2022-03-12 17:48:56 +0000

    Adding headers and starting the client class

--- include/client.h
+++ include/client.h
@@ -1,5 +1,28 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+// Adding the libraries
+#include "crypto.h"
+#include <iostream>
+// #include <string>
 
-#endif //CLIENT_H
+class Server;
+
+class Client {
+public:
+    Client(std::string _id, const Server& _server);
+    std::string get_id() const;
+    std::string get_publickey() const;
+    double get_wallet();
+    std::string sign(std::string txt);
+    bool transfer_money(std::string receiver, double value);
+    size_t generate_nonce();
+
+private:
+    Server const* const server;
+    const std::string id;
+    std::string public_key;
+    std::string private_key;
+};
+
+#endif // CLIENT_H
--- src/client.cpp
+++ src/client.cpp
@@ -1,1 +1,38 @@
 #include "client.h"
+
+Client::Client(std::string _id, const Server& _server)
+
+    : id { _id }
+    , server { &_server }
+{
+    /**
+     * @brief Construct a new Client:: Client object
+     *
+     * @param _id The id of the client
+     * @param _server The server object
+     */
+    crypto::generate_key(public_key, private_key);
+}
+std::string Client::get_id() const
+{
+    /**
+     * @brief Get the id string
+     * @return std::string
+     */
+    return id;
+}
+std::string Client::get_publickey() const
+{
+    /**
+     * @brief Get the publickey string
+     * @return std::string
+     */
+    return public_key;
+}
+// double Client::get_wallet()
+// {
+//     /**
+//      * @brief Get the wallet double
+//      * @return double
+//      */
+// }
